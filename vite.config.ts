2edcaefe207baa3df75b6981ced5119395e4017c
bharani <bharanidharan.m632005@gmail.com>
2025-08-07 22:58:01 +0530

    tio

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/renacod.com/',
   server: {
     host: "::",
     port: 8080,
